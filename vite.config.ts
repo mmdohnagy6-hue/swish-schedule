92953cdb64326bce401e53389fab83871f7434f9
Mamdouh Nagy <mmdohnagy6@gmail.com>
2026-02-19 13:33:04 +0200

    Fix xlsx issue for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['xlsx']
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    commonjsOptions: {
+      include: [/xlsx/, /node_modules/]
+    },
     rollupOptions: {
       input: {
         main: './index.html'
